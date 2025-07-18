1edbaa47d49f3e486cc690e6a8a5e8e03137ddb3
Rohit Ramesh Ramani <rohitramesh74@gmail.com>
2025-07-19 01:44:05 +0530

    Initial commit with Qt MiniGCS

---
+++ build/Desktop_Qt_6_5_0_MSVC2019_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,12 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_CXX_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Preview/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Preview/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/Qt/6.5.0/msvc2019_64" CACHE "PATH" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "D:/Qt/MaintenanceTool.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/GIT Repos/IIT_B/MiniGCS/build/Desktop_Qt_6_5_0_MSVC2019_64bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/Qt/6.5.0/msvc2019_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
